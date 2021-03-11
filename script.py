0c584e131e5165fa0032a532fda5d70af790c378
naseef1235 <80143146+naseef1235@users.noreply.github.com>
2021-03-11 07:36:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,64 +3,31 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+Please go to @CFCHART to use me<i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 
 <i>
-* Add bot to your group with admin rights.
+* First go to @CFCHART
 
-* Add bot to channels which you want to link with <b>all admin rights</b>!
+*Type movie name. NB: first search movie name in @imdb then copy and paste. If Movie available i Will send to you.
 </i>
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
+<b>
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+<b>© @CFCHATOFFICIAL</b>"""
 
 
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : {}</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @CFCHART 
 
-⭕️<b>Language :</b> <code>Python3</code>
+⭕️<b>Language :</b> <code>Manglish</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>website 1:</b> <a href='https://edapazhassimedia.blogspot.com'>movie website</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>website 2:</b> <a href='https://musicder.ml'>music website</a> 
 
 """
